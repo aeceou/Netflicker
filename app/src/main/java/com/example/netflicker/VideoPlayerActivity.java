c95a6ccc7d3d088fa15bd8e989e687b9ac091f64
mjlee777 <62866356+mjlee777@users.noreply.github.com>
2021-12-22 20:22:12 +0900

    Implement subtitles

--- app/src/main/java/com/example/netflicker/VideoPlayerActivity.java
+++ app/src/main/java/com/example/netflicker/VideoPlayerActivity.java
@@ -1,27 +1,53 @@
 package com.example.netflicker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Icon;
+import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.developer.filepicker.controller.DialogSelectionListener;
+import com.developer.filepicker.model.DialogConfigs;
+import com.developer.filepicker.model.DialogProperties;
+import com.developer.filepicker.view.FilePickerDialog;
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.PlaybackParameters;
 import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.MergingMediaSource;
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.source.SingleSampleMediaSource;
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.MimeTypes;
 import com.google.android.exoplayer2.util.Util;
+import com.google.common.io.LineReader;
 
 import java.io.File;
 import java.util.ArrayList;
 
-public class VideoPlayerActivity extends AppCompatActivity {
+public class VideoPlayerActivity extends AppCompatActivity implements View.OnClickListener {
 
     ArrayList<MediaFiles> mVideoFiles = new ArrayList<>();
     PlayerView playerView;
@@ -29,22 +55,104 @@ public class VideoPlayerActivity extends AppCompatActivity {
     int position;
     String videoTitle;
     TextView title;
+    private ControlsMode controlsMode;
+    public enum ControlsMode {
+        LOCK,FULLSCREEN;
+    }
+    ImageView videoBack, lock, unlock, scaling;
+    RelativeLayout root;
     ConcatenatingMediaSource concatenatingMediaSource;
+    //horizontal recyclerview variables
+    private ArrayList<IconModel> iconModelArrayList = new ArrayList<>();
+    PlaybackIconsAdapter playbackIconsAdapter;
+    RecyclerView recyclerViewIcons;
+    boolean expand = false;
+    PlaybackParameters parameters;
+    DialogProperties dialogProperties;
+    FilePickerDialog filePickerDialog;
+    Uri uriSubtitle;
+
+    //horizontal recyclerview variables
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setFullScreen();
         setContentView(R.layout.activity_video_player);
-        playerView = findViewById(R.id.exoplayer_view);
         getSupportActionBar().hide();
+        playerView = findViewById(R.id.exoplayer_view);
         position = getIntent().getIntExtra("position", 1);
         videoTitle = getIntent().getStringExtra("video_title");
         mVideoFiles = getIntent().getExtras().getParcelableArrayList("videoArrayList");
+        //screenOrientation();
 
         title = findViewById(R.id.video_title);
+        videoBack = findViewById(R.id.video_back);
+        lock = findViewById(R.id.lock);
+        unlock = findViewById(R.id.unlock);
+        scaling = findViewById(R.id.scaling);
+        root = findViewById(R.id.root_layout);
+        recyclerViewIcons = findViewById(R.id.recyclerview_icon);
+
         title.setText(videoTitle);
-        
+
+        videoBack.setOnClickListener(this);
+        lock.setOnClickListener(this);
+        unlock.setOnClickListener(this);
+        scaling.setOnClickListener(firstListener);
+
+        dialogProperties = new DialogProperties();
+        filePickerDialog = new FilePickerDialog(VideoPlayerActivity.this);
+        filePickerDialog.setTitle("Select a Subtitle File");
+        filePickerDialog.setPositiveBtnName("OK");
+        filePickerDialog.setNegativeBtnName("Cancel");
+
+        //iconModelArrayList.add(new IconModel(R.drawable.ic_right, ""));
+        iconModelArrayList.add(new IconModel(R.drawable.ic_rotate, "Rotate"));
+        iconModelArrayList.add(new IconModel(R.drawable.ic_subtitle, "Subtitle"));
+
+        playbackIconsAdapter = new PlaybackIconsAdapter(iconModelArrayList, this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this,
+                RecyclerView.HORIZONTAL, true);
+        recyclerViewIcons.setLayoutManager(layoutManager);
+        recyclerViewIcons.setAdapter(playbackIconsAdapter);
+        playbackIconsAdapter.notifyDataSetChanged();
+        playbackIconsAdapter.setOnItemClickListener(new PlaybackIconsAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                if (position == 0) {
+                    if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+                        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                        playbackIconsAdapter.notifyDataSetChanged();
+                    } else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                        playbackIconsAdapter.notifyDataSetChanged();
+                    }
+                }
+                if (position == 1) {
+                    dialogProperties.selection_mode = DialogConfigs.SINGLE_MODE;
+                    dialogProperties.extensions = new String[]{".srt"};
+                    dialogProperties.root = new File("/storage/emulated/0");
+                    filePickerDialog.setProperties(dialogProperties);
+                    filePickerDialog.show();
+                    filePickerDialog.setDialogSelectionListener(new DialogSelectionListener() {
+                        @Override
+                        public void onSelectedFilePaths(String[] files) {
+                            for (String path : files) {
+                                File file = new File(path);
+                                uriSubtitle = Uri.parse(file.getAbsolutePath().toString());
+                                //Log.e("20202989", file.getAbsolutePath());
+                            }
+                            playVideoSubtitle(uriSubtitle);
+                        }
+                    });
+                }
+
+            }
+        });
+
         playVideo();
     }
+
 
     private void playVideo() {
         String path = mVideoFiles.get(position).getPath();
@@ -61,10 +169,62 @@ public class VideoPlayerActivity extends AppCompatActivity {
         }
         playerView.setPlayer(player);
         playerView.setKeepScreenOn(true);
+        //player.setPlaybackParameters(parameters);
         player.prepare(concatenatingMediaSource);
         player.seekTo(position, C.TIME_UNSET);
         playError();
 
+    }
+
+    private void playVideoSubtitle(Uri subtitle) {
+        long oldPosition = player.getCurrentPosition();
+        player.stop();
+
+        String path = mVideoFiles.get(position).getPath();
+        Uri uri = Uri.parse(path);
+        player = new SimpleExoPlayer.Builder(this).build();
+        DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(
+                this, Util.getUserAgent(this, "app"));
+        concatenatingMediaSource = new ConcatenatingMediaSource();
+        for (int i = 0; i < mVideoFiles.size(); i++) {
+            new File(String.valueOf(mVideoFiles.get(i)));
+            MediaSource mediaSource = new ProgressiveMediaSource.Factory(dataSourceFactory)
+                    .createMediaSource(Uri.parse(String.valueOf(uri)));
+            Format textFormat = Format.createTextSampleFormat(null, MimeTypes.APPLICATION_SUBRIP, Format.NO_VALUE, "app");
+            MediaSource subtitleSource = new SingleSampleMediaSource.Factory(dataSourceFactory).setTreatLoadErrorsAsEndOfStream(true)
+                    .createMediaSource(Uri.parse(String.valueOf(subtitle)), textFormat, C.TIME_UNSET);
+            MergingMediaSource mergingMediaSource = new MergingMediaSource(mediaSource, subtitleSource);
+            concatenatingMediaSource.addMediaSource(mergingMediaSource);
+        }
+        playerView.setPlayer(player);
+        playerView.setKeepScreenOn(true);
+        //player.setPlaybackParameters(parameters);
+        player.prepare(concatenatingMediaSource);
+        player.seekTo(position, oldPosition);
+        playError();
+
+    }
+
+    private void screenOrientation() {
+        try {
+
+            MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+            Bitmap bitmap;
+            String path = mVideoFiles.get(position).getPath();
+            Uri uri = Uri.parse(path);
+            retriever.setDataSource(this, uri);
+            bitmap = retriever.getFrameAtTime();
+
+            int videoWidth = bitmap.getWidth();
+            int videoHeight = bitmap.getHeight();
+            if (videoWidth < videoHeight) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            } else {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+        } catch (Exception e) {
+            Log.e("MediaMetaDataRetriever", "screenOrientation");
+        }
     }
 
     private void playError() {
@@ -76,4 +236,98 @@ public class VideoPlayerActivity extends AppCompatActivity {
         });
         player.setPlayWhenReady(true);
     }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if (player.isPlaying()) {
+            player.stop();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        player.setPlayWhenReady(false);
+        player.getPlaybackState();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        player.setPlayWhenReady(true);
+        player.getPlaybackState();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        player.setPlayWhenReady(true);
+        player.getPlaybackState();
+    }
+
+    private void setFullScreen() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.video_back:
+                if (player != null) {
+                    player.release();
+                }
+                finish();
+                break;
+            case R.id.lock:
+                controlsMode = ControlsMode.FULLSCREEN;
+                root.setVisibility(View.VISIBLE);
+                lock.setVisibility(View.INVISIBLE);
+                Toast.makeText(this, "unLocked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.unlock:
+                controlsMode = ControlsMode.LOCK;
+                root.setVisibility(View.INVISIBLE);
+                lock.setVisibility(View.VISIBLE);
+                Toast.makeText(this, "Locked", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+    }
+
+    View.OnClickListener firstListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            playerView.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FILL);
+            player.setVideoScalingMode(C.VIDEO_SCALING_MODE_DEFAULT);
+            scaling.setImageResource(R.drawable.fullscreen);
+
+            Toast.makeText(VideoPlayerActivity.this, "Full Screen", Toast.LENGTH_SHORT).show();
+            scaling.setOnClickListener(secondListener);
+        }
+    };
+    View.OnClickListener secondListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            playerView.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_ZOOM);
+            player.setVideoScalingMode(C.VIDEO_SCALING_MODE_DEFAULT);
+            scaling.setImageResource(R.drawable.zoom);
+
+            Toast.makeText(VideoPlayerActivity.this, "Zoom", Toast.LENGTH_SHORT).show();
+            scaling.setOnClickListener(thirdListener);
+        }
+    };
+    View.OnClickListener thirdListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            playerView.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FIT);
+            player.setVideoScalingMode(C.VIDEO_SCALING_MODE_DEFAULT);
+            scaling.setImageResource(R.drawable.fit);
+
+            Toast.makeText(VideoPlayerActivity.this, "Fit", Toast.LENGTH_SHORT).show();
+            scaling.setOnClickListener(firstListener);
+        }
+    };
 }
