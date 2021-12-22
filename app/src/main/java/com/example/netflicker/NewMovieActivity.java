8528e1c2c3c5ebdb3c7d1508f2602ab65089582b
mjlee777 <62866356+mjlee777@users.noreply.github.com>
2021-12-23 02:32:25 +0900

    [02:31] User vocab list implemented.

---
+++ app/src/main/java/com/example/netflicker/NewMovieActivity.java
@@ -0,0 +1,15 @@
+package com.example.netflicker;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewMovieActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_movie);
+    }
+}
